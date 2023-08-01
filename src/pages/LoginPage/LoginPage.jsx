57e605744872ae79c2acce046208bc47ea5d6d4d
djones1117 <socjones21@yahoo.com>
2023-08-01 14:59:05 -0400

    POST /api/users/login 200

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,12 +1,97 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './LoginPage.css';
 
 
-export default function LoginPage(props){
-   
 
+import { Link, useNavigate} from 'react-router-dom'
+
+import {
+	Button,
+	Form,
+	Grid,
+	Header,
+	Image,
+	Message,
+	Segment,
+  } from "semantic-ui-react";
+ 
+  import userService from '../../utils/userService';
+
+
+
+export default function LoginPage({handleSignUpOrLogin}) {
+   
+  const [state, setState] = useState({
+    email: '',
+    password: ''
+  })
+const [error, setError] = useState('')
+
+const navigate = useNavigate();
+
+
+async function handleSubmit(e){
+  e.preventDefault();
+
+
+try {
+  await userService.login(state)
+  navigate('/')
+  handleSignUpOrLogin();
+
+
+} catch(err){
+  console.log(err)
+  setError('check terminal and console')
+}
+
+
+
+}
+
+function handleChange(e){
+  setState({
+    ...state,
+    [e.target.name]: e.target.value
+  })
+}
     return (
-      <div>Login Pageeeeee</div>
-      );
+      <Grid textAlign="center" style={{ height: "100vh" }} verticalAlign="middle">
+      <Grid.Column style={{ maxWidth: 450 }}>
+        <Header as="h2" color="green" textAlign="center">
+          <Image src="https://imgur.com/KjeX81S.jpg" /> Login
+        </Header>
+        <Form autoComplete="off" onSubmit={handleSubmit}>
+          <Segment stacked>
+            <Form.Input
+              type="email"
+              name="email"
+              placeholder="email"
+              value={state.email}
+              onChange={handleChange}
+              required
+            />
+            <Form.Input
+              name="password"
+              type="password"
+              placeholder="password"
+              value={state.password}
+              onChange={handleChange}
+              required
+            />
+
+            <Button type="submit" className="btn">
+              Login
+            </Button>
+          </Segment>
+          <Message>
+            Don't have an Account? <Link to="/signup">Sign up</Link>
+          </Message>
+          {error ? <ErrorMessage error={error} /> : null}
+        </Form>
+      </Grid.Column>
+      </Grid>
+);
+  
 }
 
