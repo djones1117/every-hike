b56b322e68a74dd287b532a4d756ba23458af043
djones1117 <socjones21@yahoo.com>
2023-08-02 14:13:22 -0400

    hiking post model complete

---
+++ models/post.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const favoritesSchema = mongoose.Schema({
+    username: String,
+    //One user can have many favorites
+
+    userId: { type: mongoose.Schema.Types.ObjectId }
+})
+//one post can have many favorites. one user can have many posts.
+const postSchema = new mongoose.Schema({
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+    photoUrl: String,
+    difficulty: String, enum: ["Easy", "Moderate", "Hard"],
+    trail: String, 
+    location: String,
+    length: String,
+    favorites: [favoritesSchema]
+})
+
+module.exports = mongoose.model('Post', postSchema);
