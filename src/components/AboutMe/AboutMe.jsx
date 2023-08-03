8302bfe1c4f1ee984baeca8e0cc4b2719a5178c7
djones1117 <socjones21@yahoo.com>
2023-08-03 17:03:32 -0400

    can render each users profile page

---
+++ src/components/AboutMe/AboutMe.jsx
@@ -0,0 +1,9 @@
+import {  Image, Grid, Segment } from 'semantic-ui-react';
+
+function AboutMe(){
+    return(
+<h1>About me area</h1>
+    );
+}
+
+export default AboutMe
--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,7 +1,30 @@
+import AboutMe from "../../components/AboutMe/AboutMe";
+import HikeGallery from "../../components/HikeGallery/HikeGallery";
+import { Grid } from "semantic-ui-react";
+import PageHeader from "../../components/Header/Header";
+import React, { useState, useEffect } from "react";
 
+export default function ProfilePage() {
+   
+    const [posts, setPosts] = useState([]);
 
-export default function ProfilePage(){
-    return(
-<h1>This is the profile page </h1>
-    );
+  return (
+    <Grid>
+      <Grid.Row>
+        <Grid.Column>
+          <PageHeader />
+        </Grid.Column>
+      </Grid.Row>
+      <Grid.Row>
+        <Grid.Column>
+          <AboutMe />
+        </Grid.Column>
+      </Grid.Row>
+      <Grid.Row centered>
+        <Grid.Column style={{ maxWidth: 750 }}>
+          <HikeGallery posts={posts}/>
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+  );
 }
