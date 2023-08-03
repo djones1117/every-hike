e12cc304347ebf44adbffe4f52536180fdea79ea
djones1117 <socjones21@yahoo.com>
2023-08-03 10:46:06 -0400

    added hike post form

--- src/components/AddHikeForm/AddHikeForm.jsx
+++ src/components/AddHikeForm/AddHikeForm.jsx
@@ -1,5 +1,93 @@
+import React, { useState } from "react";
+import { Button, Form, Grid, Segment } from "semantic-ui-react";
+
+
+
+
 export default function AddHikeForm(){
-    return(
-        <span> Post Form</span>
+
+
+const [state, setState] = useState({
+    difficulty: '',
+    trail: '',
+    length: '',
+    location: ''
+
+})
+
+const [selectedFile, setSelectedFile] = useState('');
+
+function handleFileInput(e){
+    setSelectedFile(e.target.files[0])
+}
+
+
+
+function handleChange(e){
+    setState({
+        ...state,
+        [e.target.name]: e.target.value
+    })
+}
+
+
+function handleSubmit(e)  {
+    const formData = new FormData()
+    formData.append('difficulty', state.difficulty)
+    formData.append('trail', state.trail)
+    formData.append('length', state.length)
+    formData.append('location', state.location)
+    formData.append('photo', selectedFile)
+}
+    
+
+
+return(
+        <Segment>
+        <Form autoComplete="off" onSubmit={handleSubmit}>
+          <Form.Input
+            className="form-control"
+            name="difficulty"
+            value={state.difficulty}
+            placeholder="Easy, Moderate, Hard"
+            onChange={handleChange}
+            required
+          />
+          <Form.Input
+          className="form-control"
+          name="trail"
+          value={state.trail}
+          placeholder="Trail Name"
+          onChange={handleChange}
+          required
+          />
+          <Form.Input
+          className="form-control"
+          name="length"
+          value={state.length}
+          placeholder="Length"
+          onChange={handleChange}
+          required
+          />
+          <Form.Input
+          className="form-control"
+          name="location"
+          value={state.location}
+          placeholder="Hike Location"
+          onChange={handleChange}
+          required
+          />
+          <Form.Input
+            className="form-control"
+            type="file"
+            name="photo"
+            placeholder="upload image"
+            onChange={handleFileInput}
+          />
+          <Button type="submit" className="btn">
+            ADD Hike
+          </Button>
+        </Form>
+      </Segment>
     );
 }
---
+++ src/utils/postApi.js
@@ -0,0 +1,23 @@
+import tokenService from "./tokenService";
+
+const BASE_URL = '/api/posts/';
+
+
+export function create(data){
+	return fetch(BASE_URL, {
+		method: 'POST',
+		body: data, 
+		headers: {
+			
+			
+			Authorization: "Bearer " + tokenService.getToken() // < this is how we get the token from localstorage and send it to our api request
+			// so the server knows who the request is coming from when the client is trying to make a POST
+		}
+	}).then(responseFromTheServer => {
+		if(responseFromTheServer.ok) return responseFromTheServer.json() 
+
+
+		throw new Error('Something wrong in create Post'); 
+		
+	})
+}
